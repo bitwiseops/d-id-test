77def8b9b29ce19944cfe73ec3102d4181d3b3e9
Flavio C <ing.flavio.cordari@gmail.com>
2023-10-07 12:15:39 +0200

    feat: starts chat implementation

--- streaming-client-api.js
+++ streaming-client-api.js
@@ -1,6 +1,13 @@
 'use strict';
 import DID_API from './api.json' assert { type: 'json' };
 
+const QUESTIONS = [
+  {question: "Chi può accedere all’elenco dei delegati alle vendite?", answer: "Si ritiene che solo l’iscritto alla sezione A dell’albo possa svolgere la funzione di delegato alle vendite"},
+  {question: "Chi ha predisposto le linee guida generali per la formazione dei delegati?", answer: "La Scuola superiore della magistratura e la formazione dei professionisti che provvedono alle operazioni di vendita."},
+  {question: "Ai fini del positivo superamento della prova finale di esame del corso di formazione per i delegati alle vendite, quante devono essere le risposte esatte?", answer: "Il test sarà considerato superato rispondendo correttamente ad almeno 35 domande."},
+  {question: "Secondo quanto previsto dall’art. 179 - ter disp. att. c.p.c. quanti incarichi deve aver svolto il professionista delegato alle operazioni di vendita nel quinquennio precedente per essere iscritto nell’elenco dei delegati?", answer: "Ai fini della dimostrazione della specifica competenza tecnica per la prima iscrizione nell’elenco è richiesto lo svolgimento nel quinquennio precedente di non meno di dieci incarichi di professionista delegato alle operazioni di vendita, senza che la delega sia stata revocata in conseguenza del mancato rispetto dei termini o delle direttive stabilite dal giudice dell’esecuzione"},
+]
+
 if (DID_API.key == '🤫') alert('Please put your api key inside ./api.json and restart..');
 
 const RTCPeerConnection = (
@@ -20,14 +27,18 @@ let lastBytesReceived;
 
 const talkVideo = document.getElementById('talk-video');
 talkVideo.setAttribute('playsinline', '');
-const peerStatusLabel = document.getElementById('peer-status-label');
-const iceStatusLabel = document.getElementById('ice-status-label');
-const iceGatheringStatusLabel = document.getElementById('ice-gathering-status-label');
-const signalingStatusLabel = document.getElementById('signaling-status-label');
-const streamingStatusLabel = document.getElementById('streaming-status-label');
 
-const connectButton = document.getElementById('connect-button');
-connectButton.onclick = async () => {
+talkVideo.addEventListener('loadedmetadata', function () {
+  console.log('Remote video videoWidth: ' + this.videoWidth + 'px,  videoHeight: ' + this.videoHeight + 'px');
+});
+
+// const peerStatusLabel = document.getElementById('peer-status-label');
+// const iceStatusLabel = document.getElementById('ice-status-label');
+// const iceGatheringStatusLabel = document.getElementById('ice-gathering-status-label');
+// const signalingStatusLabel = document.getElementById('signaling-status-label');
+// const streamingStatusLabel = document.getElementById('streaming-status-label');
+
+const connect = async () => {
   if (peerConnection && peerConnection.connectionState === 'connected') {
     return;
   }
@@ -42,7 +53,7 @@ connectButton.onclick = async () => {
       'Content-Type': 'application/json',
     },
     body: JSON.stringify({
-      source_url: 'https://webtoup.it/AI/avatar/avatar.jpeg',
+      source_url: 'https://webtoup.it/AI/avatar/avatar_cut.jpeg',
     }),
   });
 
@@ -72,30 +83,60 @@ connectButton.onclick = async () => {
   });
 };
 
-const talkButton = document.getElementById('talk-button');
-talkButton.onclick = async () => {
-  // connectionState not supported in firefox
-  if (peerConnection?.signalingState === 'stable' || peerConnection?.iceConnectionState === 'connected') {
-    const talkResponse = await fetchWithRetries(`${DID_API.url}/talks/streams/${streamId}`, {
-      method: 'POST',
-      headers: {
-        Authorization: `Basic ${DID_API.key}`,
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        script: {
-          type: 'audio',
-          audio_url: 'https://webtoup.it/AI/avatar/risposta1.mp3',
-        },
-        driver_url: 'bank://lively/',
-        config: {
-          stitch: true,
-        },
-        session_id: sessionId,
-      }),
-    });
+document.getElementById('send-button').addEventListener('click', function () {
+  const userInput = document.getElementById('user-input');
+  const chatBox = document.getElementById('chat-box');
+
+  if (userInput.value.trim() !== '') {
+    // Display user's message
+    const userMessage = document.createElement('div');
+    userMessage.classList.add('message', 'user');
+    userMessage.textContent = userInput.value;
+    chatBox.appendChild(userMessage);
+
+    // Example response from the bot
+    // You can replace this with an actual API call to ChatGPT or any other service
+    const botMessage = document.createElement('div');
+    botMessage.classList.add('message', 'bot');
+    botMessage.textContent = "I'm a simple example and can't generate real replies yet.";
+    chatBox.appendChild(botMessage);
+
+    // Clear the input and scroll to the latest message
+    userInput.value = '';
+    chatBox.scrollTop = chatBox.scrollHeight;
   }
-};
+});
+
+[
+  document.getElementById('talk-button-1'),
+  document.getElementById('talk-button-2'),
+  document.getElementById('talk-button-3'),
+  document.getElementById('talk-button-4'),
+].forEach((e, i) => {
+  e.onclick = async () => {
+    // connectionState not supported in firefox
+    if (peerConnection?.signalingState === 'stable' || peerConnection?.iceConnectionState === 'connected') {
+      const talkResponse = await fetchWithRetries(`${DID_API.url}/talks/streams/${streamId}`, {
+        method: 'POST',
+        headers: {
+          Authorization: `Basic ${DID_API.key}`,
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          script: {
+            type: 'audio',
+            audio_url: `https://webtoup.it/AI/avatar/risposta${i + 1}.mp3`,
+          },
+          driver_url: 'bank://lively/',
+          config: {
+            stitch: true,
+          },
+          session_id: sessionId,
+        }),
+      });
+    }
+  };
+});
 
 const destroyButton = document.getElementById('destroy-button');
 destroyButton.onclick = async () => {
@@ -113,9 +154,11 @@ destroyButton.onclick = async () => {
 };
 
 function onIceGatheringStateChange() {
-  iceGatheringStatusLabel.innerText = peerConnection.iceGatheringState;
-  iceGatheringStatusLabel.className = 'iceGatheringState-' + peerConnection.iceGatheringState;
+  // iceGatheringStatusLabel.innerText = peerConnection.iceGatheringState;
+  // iceGatheringStatusLabel.className = 'iceGatheringState-' + peerConnection.iceGatheringState;
+  console.log('ICE Gathering State changed to [', peerConnection.iceGatheringState, ']');
 }
+
 function onIceCandidate(event) {
   console.log('onIceCandidate', event);
   if (event.candidate) {
@@ -137,8 +180,9 @@ function onIceCandidate(event) {
   }
 }
 function onIceConnectionStateChange() {
-  iceStatusLabel.innerText = peerConnection.iceConnectionState;
-  iceStatusLabel.className = 'iceConnectionState-' + peerConnection.iceConnectionState;
+  // iceStatusLabel.innerText = peerConnection.iceConnectionState;
+  // iceStatusLabel.className = 'iceConnectionState-' + peerConnection.iceConnectionState;
+  console.log('ICE Connection State changed to [', peerConnection.iceConnectionState, ']');
   if (peerConnection.iceConnectionState === 'failed' || peerConnection.iceConnectionState === 'closed') {
     stopAllStreams();
     closePC();
@@ -146,26 +190,32 @@ function onIceConnectionStateChange() {
 }
 function onConnectionStateChange() {
   // not supported in firefox
-  peerStatusLabel.innerText = peerConnection.connectionState;
-  peerStatusLabel.className = 'peerConnectionState-' + peerConnection.connectionState;
+  console.log('Connection State changed to [', peerConnection.connectionState, ']');
+  // peerStatusLabel.innerText = peerConnection.connectionState;
+  // peerStatusLabel.className = 'peerConnectionState-' + peerConnection.connectionState;
 }
 function onSignalingStateChange() {
-  signalingStatusLabel.innerText = peerConnection.signalingState;
-  signalingStatusLabel.className = 'signalingState-' + peerConnection.signalingState;
+  // signalingStatusLabel.innerText = peerConnection.signalingState;
+  // signalingStatusLabel.className = 'signalingState-' + peerConnection.signalingState;
+  console.log('Signaling State changed to [', peerConnection.signalingState, ']');
 }
 
 function onVideoStatusChange(videoIsPlaying, stream) {
   let status;
   if (videoIsPlaying) {
     status = 'streaming';
+    console.log(stream);
+    talkVideo.style.transform = "scaleX(1.66) scaleY(1.16)"
     const remoteStream = stream;
     setVideoElement(remoteStream);
   } else {
+    talkVideo.style.transform = "scale(1)"
     status = 'empty';
     playIdleVideo();
   }
-  streamingStatusLabel.innerText = status;
-  streamingStatusLabel.className = 'streamingState-' + status;
+  // streamingStatusLabel.innerText = status;
+  // streamingStatusLabel.className = 'streamingState-' + status;
+  console.log('Video Status changed to [', status, ']');
 }
 
 function onTrack(event) {
@@ -174,7 +224,7 @@ function onTrack(event) {
    * that's being streamed - It does so by periodically looking for changes in total stream data size
    *
    * This information in our case is used in order to show idle video while no talk is streaming.
-   * To create this idle video use the POST https://api.d-id.com/talks endpoint with a silent audio file or a text script with only ssml breaks 
+   * To create this idle video use the POST https://api.d-id.com/talks endpoint with a silent audio file or a text script with only ssml breaks
    * https://docs.aws.amazon.com/polly/latest/dg/supportedtags.html#break-tag
    * for seamless results use `config.fluent: true` and provide the same configuration as the streaming video
    */
@@ -236,7 +286,7 @@ function setVideoElement(stream) {
 
 function playIdleVideo() {
   talkVideo.srcObject = undefined;
-  talkVideo.src = 'or_idle.mp4';
+  talkVideo.src = 'or_idle_cut.mp4';
   talkVideo.loop = true;
 }
 
@@ -259,10 +309,10 @@ function closePC(pc = peerConnection) {
   pc.removeEventListener('signalingstatechange', onSignalingStateChange, true);
   pc.removeEventListener('track', onTrack, true);
   clearInterval(statsIntervalId);
-  iceGatheringStatusLabel.innerText = '';
-  signalingStatusLabel.innerText = '';
-  iceStatusLabel.innerText = '';
-  peerStatusLabel.innerText = '';
+  // iceGatheringStatusLabel.innerText = '';
+  // signalingStatusLabel.innerText = '';
+  // iceStatusLabel.innerText = '';
+  // peerStatusLabel.innerText = '';
   console.log('stopped peer connection');
   if (pc === peerConnection) {
     peerConnection = null;
@@ -288,3 +338,14 @@ async function fetchWithRetries(url, options, retries = 1) {
     }
   }
 }
+
+document.getElementById('startButton').onclick = async () => {
+  document.getElementById('startButton').classList.add('hidden');
+  document.getElementById('loading').classList.remove('hidden');
+  await connect();
+  document.getElementById('loading').classList.add('hidden');
+  document.getElementById('content').classList.remove('hidden');
+  document.getElementById('buttons').classList.remove('hidden');
+  playIdleVideo();
+  talkVideo.play();
+};
